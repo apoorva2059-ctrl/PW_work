f3529292567ba0339433aa0b258cac5240c15def
apoorva2059-ctrl <apoorva2059@gmail.com>
2026-06-02 16:24:55 +0530

    Add test_variables.php for PHP variable examples
    This file demonstrates variable declarations in PHP, including string, integer, float, boolean, array, null, and a class with a public property.

---
+++ php_assignments/Day01/test_variables.php
@@ -0,0 +1,37 @@
+<?php
+
+$name = "Apoorva"; //string
+echo $name;
+echo "<br>";
+
+$age = 20; //integer
+echo $age;
+echo "<br>";
+
+$percentage = 48.5; //float
+echo $percentage;
+echo "<br>";
+
+$Condition = true; //boolean
+echo $Condition;
+echo "<br>";
+
+$colors = array("white", "yellow", "purple"); //array
+echo $colors[0];
+echo "<br>";
+echo $colors[1];
+echo "<br>";
+echo $colors[2];
+echo "<br>";
+
+$test = NULL; //null
+echo "NULL: ";
+echo "<br>";
+
+class Work {
+public $WorkName = "Sambhavikaa";
+}
+
+$obj = new Work();
+echo $obj->WorkName;
+?>
